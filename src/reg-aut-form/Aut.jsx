3dbdee8db06dac38042535196b8e79a28351a9ee
Roman <118013817+Miku-Nakano8888@users.noreply.github.com>
2023-07-14 17:23:53 +0300

    sw hm

--- src/reg-aut-form/Aut.jsx
+++ src/reg-aut-form/Aut.jsx
@@ -3,17 +3,17 @@ import styles from './Reg.module.css'
 export default function AutForm(){
     return(
        <div className={styles.regform}>
-        <h3>AUTHORISATION</h3>
+        <h3>Авторизация</h3>
         <form className={styles.form}>
             <label>
                 Email:
                 <input type="text" name="email" placeholder='example@gmail.com'/>
              </label>
              <label>
-                Password:
-                <input type="password" name="password" placeholder='**********'/>
+                Пароль:
+                <input type="password" name="password" placeholder=''/>
              </label>
-            <input type="submit" value="SIGN IN" className={styles.but}/>
+            <input type="submit" value="Войти" className={styles.but}/>
         </form>
         </div> 
     )
--- src/reg-aut-form/Reg.jsx
+++ src/reg-aut-form/Reg.jsx
@@ -3,17 +3,17 @@ import styles from './Reg.module.css'
 export default function RegForm(){
     return(
        <div className={styles.regform}>
-        <h3>AUTHENTICATION</h3>
+        <h3>Регистрация</h3>
         <form className={styles.form}>
             <label>
                 Email:
                 <input type="text" name="email" placeholder='example@gmail.com'/>
              </label>
              <label>
-                Password:
-                <input type="password" name="password" placeholder='**********'/>
+                Пароль:
+                <input type="password" name="password" placeholder=''/>
              </label>
-            <input type="submit" value="LOG IN" className={styles.but}/>
+            <input type="submit" value="Зарегистрироваться" className={styles.but}/>
         </form>
         </div> 
     )
